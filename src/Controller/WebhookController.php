e5abd7df084479d80ea016b9c51edcc4b9eb71ac
Neil Peyssard <neil@sherlockode.fr>
2022-09-12 15:56:12 +0200

    Use state machine for payment states in webhooks

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -5,10 +5,11 @@ namespace Sherlockode\SyliusCheckoutPlugin\Controller;
 use Doctrine\ORM\EntityManagerInterface;
 use Sherlockode\SyliusCheckoutPlugin\Checkout\Factory\ClientFactory;
 use Sherlockode\SyliusCheckoutPlugin\Checkout\Model\Charge;
+use SM\Factory\FactoryInterface;
 use Sylius\Component\Core\Model\OrderInterface;
-use Sylius\Component\Core\OrderPaymentStates;
 use Sylius\Component\Order\Repository\OrderRepositoryInterface;
 use Sylius\Component\Payment\Model\PaymentInterface;
+use Sylius\Component\Payment\PaymentTransitions;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
@@ -25,6 +26,11 @@ class WebhookController
     private $em;
 
     /**
+     * @var FactoryInterface
+     */
+    private $stateMachineFactory;
+
+    /**
      * @var OrderRepositoryInterface
      */
     private $orderRepository;
@@ -38,15 +44,18 @@ class WebhookController
      * WebhookController constructor.
      *
      * @param EntityManagerInterface   $em
+     * @param FactoryInterface         $stateMachineFactory
      * @param OrderRepositoryInterface $orderRepository
      * @param ClientFactory            $clientFactory
      */
     public function __construct(
         EntityManagerInterface $em,
+        FactoryInterface $stateMachineFactory,
         OrderRepositoryInterface $orderRepository,
         ClientFactory $clientFactory
     ) {
         $this->em = $em;
+        $this->stateMachineFactory = $stateMachineFactory;
         $this->orderRepository = $orderRepository;
         $this->clientFactory = $clientFactory;
     }
@@ -64,8 +73,7 @@ class WebhookController
         $this->verifyRequest($payment, $request);
 
         $charge = $this->getPaymentState($payment);
-        $this->setPaymentStatus($payment, $charge);
-        $this->setOrderStatus($payment->getOrder(), $charge);
+        $this->applyTransition($payment, $charge);
 
         $this->em->flush();
 
@@ -153,52 +161,34 @@ class WebhookController
     /**
      * @param PaymentInterface $payment
      * @param Charge           $charge
+     *
+     * @throws \SM\SMException
      */
-    private function setPaymentStatus(PaymentInterface $payment, Charge $charge): void
+    private function applyTransition(PaymentInterface $payment, Charge $charge): void
     {
-        $states = [
-            'Pending' => PaymentInterface::STATE_PROCESSING,
-            'Authorized' => PaymentInterface::STATE_AUTHORIZED,
-            'Card Verified' => PaymentInterface::STATE_AUTHORIZED,
-            'Voided' => PaymentInterface::STATE_CANCELLED,
-            'Partially Captured' => PaymentInterface::STATE_PROCESSING,
-            'Captured' => PaymentInterface::STATE_COMPLETED,
-            'Partially Refunded' => PaymentInterface::STATE_REFUNDED,
-            'Refunded' => PaymentInterface::STATE_REFUNDED,
-            'Declined' => PaymentInterface::STATE_FAILED,
-            'Canceled' => PaymentInterface::STATE_CANCELLED,
-            'Expired' => PaymentInterface::STATE_FAILED,
-            'Paid' => PaymentInterface::STATE_COMPLETED,
+        $transitions = [
+            'Pending' => PaymentTransitions::TRANSITION_PROCESS,
+            'Authorized' => PaymentTransitions::TRANSITION_AUTHORIZE,
+            'Card Verified' => PaymentTransitions::TRANSITION_AUTHORIZE,
+            'Voided' => PaymentTransitions::TRANSITION_CANCEL,
+            'Partially Captured' => PaymentTransitions::TRANSITION_PROCESS,
+            'Captured' => PaymentTransitions::TRANSITION_COMPLETE,
+            'Partially Refunded' => PaymentTransitions::TRANSITION_REFUND,
+            'Refunded' => PaymentTransitions::TRANSITION_REFUND,
+            'Declined' => PaymentTransitions::TRANSITION_FAIL,
+            'Canceled' => PaymentTransitions::TRANSITION_CANCEL,
+            'Expired' => PaymentTransitions::TRANSITION_FAIL,
+            'Paid' => PaymentTransitions::TRANSITION_COMPLETE,
         ];
 
-        if (isset($states[$charge->getStatus()])) {
-            $payment->setState($states[$charge->getStatus()]);
-        }
-    }
+        $transition = $transitions[$charge->getStatus()] ?? null;
 
-    /**
-     * @param OrderInterface $order
-     * @param Charge         $charge
-     */
-    private function setOrderStatus(OrderInterface $order, Charge $charge): void
-    {
-        $states = [
-            'Pending' => OrderPaymentStates::STATE_AWAITING_PAYMENT,
-            'Authorized' => OrderPaymentStates::STATE_AWAITING_PAYMENT,
-            'Card Verified' => OrderPaymentStates::STATE_AWAITING_PAYMENT,
-            'Voided' => OrderPaymentStates::STATE_AWAITING_PAYMENT,
-            'Partially Captured' => OrderPaymentStates::STATE_AWAITING_PAYMENT,
-            'Captured' => OrderPaymentStates::STATE_PAID,
-            'Partially Refunded' => OrderPaymentStates::STATE_REFUNDED,
-            'Refunded' => OrderPaymentStates::STATE_REFUNDED,
-            'Declined' => OrderPaymentStates::STATE_AWAITING_PAYMENT,
-            'Canceled' => OrderPaymentStates::STATE_AWAITING_PAYMENT,
-            'Expired' => OrderPaymentStates::STATE_AWAITING_PAYMENT,
-            'Paid' => OrderPaymentStates::STATE_PAID,
-        ];
+        if ($transition) {
+            $stateMachine = $this->stateMachineFactory->get($payment, PaymentTransitions::GRAPH);
 
-        if (isset($states[$charge->getStatus()])) {
-            $order->setPaymentState($states[$charge->getStatus()]);
+            if ($stateMachine->can($transition)) {
+                $stateMachine->apply($transition);
+            }
         }
     }
 }
